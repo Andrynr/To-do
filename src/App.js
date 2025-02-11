9ae64b08fa4a42f328b5e06b07aa473662f66512
Romuald <nomenjanaharyandry.na@gmail.com>
2025-02-11 17:27:57 +0300

    Modification des styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import "./App.css";
+import "./bootstrap.min.css";
 
 function App() {
   const [tasks, setTasks] = useState([]);
@@ -6,38 +8,68 @@ function App() {
 
   const addTask = () => {
     if (newTask.trim() !== "") {
-      setTasks([...tasks, { text: newTask, completed: false }]);
+      let temps = Date();
+      temps = temps.slice(0, 24);
+      setTasks([...tasks, { text: newTask, date: temps, completed: false }]);
       setNewTask(""); //Réinitialise l'entrée
     }
   };
 
   const completeTask = (index) => {
-    setTasks([...tasks, tasks.map((task, i) => (task.completed = i === index ? true : task))]);
-  }
-  
+    setTasks(
+      tasks.map((task, i) =>
+        i === index ? { ...task, completed: true } : task
+      )
+    );
+  };
+
   const deleteTask = (index) => {
     setTasks(tasks.filter((_, i) => i !== index));
   };
+
   return (
-    <div style={{ padding: "20px" }}>
+    <div style={{ padding: "20px" }} id="corps">
       <h1>To-Do App</h1>
       <div>
-        <input type="text" placeholder="Ajouter une tâche..." value={newTask}
-        onChange={(e) => setNewTask(e.target.value)} />
+        <input
+          type="text"
+          placeholder="Ajouter une tâche..."
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          autofocus="true"
+          capitalize="true"
+        />
         <button onClick={addTask}>Ajouter</button>
       </div>
       <ul>
         {tasks && tasks.length > 0 ? (
           tasks.map((task, index) => (
-          <li key={index} style={{ marginBottom: "10px" }}>
-            <span>{ task.text }</span>
-            <button onClick={() => deleteTask(index)}
-              style={{ marginLeft: "10px"}}>Delete
-            </button>
-            <button onClick={() => completeTask(index)} 
-            style={{ marginLeft: "10px"}}>Completed</button>
-          </li>
-        ))) : (<p>Aucune tache disponible</p>)}
+            <li
+              key={index}
+              class={task.completed ? "task complet" : "task"}
+              style={{ marginBottom: "10px" }}
+            >
+              <span>{task.text}</span>
+              <span class="date">{task.date}</span>
+              <button
+                id="delete"
+                onClick={() => deleteTask(index)}
+                style={{ marginLeft: "10px" }}
+              >
+                Delete
+              </button>
+              <button
+                id="complete"
+                onClick={() => completeTask(index)}
+                style={{ marginLeft: "10px" }}
+              >
+                Completed
+              </button>
+            </li>
+          ))
+        ) : (
+          <p>Aucune tache disponible</p>
+        )}
       </ul>
     </div>
   );
