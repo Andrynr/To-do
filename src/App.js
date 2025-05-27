520990fdd72eef3f2ff28affedb92a78fe49afc1
Romuald <nomenjanaharyandry.na@gmail.com>
2025-05-27 15:02:11 +0300

    Transformation du formulaire en composant

--- src/App.js
+++ src/App.js
@@ -1,36 +1,24 @@
 import React, { useEffect, useState, useRef } from "react";
 import "./App.css";
+import TodoForm from "./components/TodoForm";
 
 function App() {
   const id = useRef(0);
   const [tasks, setTasks] = useState([]);
-  const [newData, setNewData] = useState({
-    text: "", description: "", lieu: "", isPriority: false
-  });
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setNewData((prevData) => ({ ...prevData, [name]: value }));
-  };
-
-  const addTask = (e) => {
-    e.preventDefault();
-
-    if (!newData.text.trim()) return;
-
+  const addTask = (newData) => {
       let now = new Date();
       let temps = now.toLocaleString();
       const newTask = {
         id: id.current++,
-        text: newData.text, 
-        description: newData.description, 
+        text: newData.text.trim(), 
+        description: newData.description.trim(), 
         place: newData.lieu, 
         date: temps, 
         completed: false, 
         isPriority: newData.isPriority
       };
       setTasks([...tasks, newTask]);
-      setNewData({ text: "", description: "", lieu: "", isPriority: false}); //Réinitialise l'entrée
   };
 
   const completeTask = (index) => {
@@ -52,50 +40,8 @@ function App() {
 
   return (
     <div style={{ padding: "20px" }} id="corps">
-      <form onSubmit={addTask}>
-        <div className="row g-4">
-          <div className="col-md-6">
-            <h1 className="display-3">To-Do App</h1>
-          
-            <div className="input-group">
-              <div className="form-floating">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="..."
-                  value={newData.text}
-                  onChange={handleChange}
-                  capitalize="true"
-                  id="ajouterUneTache"
-                  name="text"
-                  autoFocus
-                  required
-                />
-                <label className="form-label" htmlFor="ajouterUneTache">
-                  Ajouter une nouvelle tache
-                </label>
-                <div className="valid-tooltip">Looks good!!</div>
-                <div className="invalid-tooltip">Try again!!</div>
-              </div>
-              <button className="btn" id="btn-ajout" type="submit" onClick={addTask}>
-                Ajouter
-             </button>
-            </div>
-          </div>
-          <button type="button" className="d-md-none dropdown-toggle btn  m-0" data-bs-toggle="collapse" data-bs-target="#detail" id="Plus"><span>Ajouter des détail</span></button>
-          <div className=" col-md-6 d-md-flex flex-column justify-content-end">
-            <div className="collapse d-md-block" id="detail">
-              <div className="form-check">
-                <input type="checkbox" className="form-check-input" checked={newData.isPriority}
-                 onChange={(e) => setNewData({ ...newData,isPriority: e.target.checked })} id="priorite" name="priorite" />
-                <label className="form-check-label" htmlFor="priorite">Prioritaire</label>
-              </div>
-              <textarea className="form-control mb-2" value={newData.description} onChange={handleChange} name="description"></textarea>
-              <input type="text" className="form-control mb-2" value={newData.lieu} onChange={handleChange} placeholder="Lieu" name="lieu" />
-            </div>
-          </div>
-        </div>
-      </form>
+      <h1 className="display-3">To-Do App</h1>
+      <TodoForm onAdd={addTask} />
       <div className=" list-taches my-md-3 container-fluide">
         <ul className="row g-3 list-unstyled">
           {tasks && tasks.length > 0 ? (
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+
+function TodoForm({ onAdd }) {
+  const [newData, setNewData] = useState({
+    text: "", description: "", lieu: "", isPriority: false
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!newData.text.trim()) return;
+
+    onAdd(newData);
+
+    setNewData({ text: "", description: "", lieu: "", isPriority: false}); //Réinitialise l'entrée
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+        <div className="row g-4">
+          <div className="col-md-6">
+            <div className="input-group">
+              <div className="form-floating">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="..."
+                  value={newData.text}
+                  onChange={handleChange}
+                  capitalize="true"
+                  id="ajouterUneTache"
+                  name="text"
+                  autoFocus
+                  required
+                />
+                <label className="form-label" htmlFor="ajouterUneTache">
+                  Ajouter une nouvelle tache
+                </label>
+                <div className="valid-tooltip">Looks good!!</div>
+                <div className="invalid-tooltip">Try again!!</div>
+              </div>
+              <button className="btn" id="btn-ajout" type="submit">
+                Ajouter
+             </button>
+            </div>
+
+          </div>
+          <button type="button" className="d-md-none dropdown-toggle btn  m-0" data-bs-toggle="collapse" data-bs-target="#detail" id="Plus"><span>Ajouter des détail</span></button>
+          <div className=" col-md-6 d-md-flex flex-column justify-content-end">
+            <div className="collapse d-md-block" id="detail">
+              <div className="form-check">
+                <input type="checkbox" className="form-check-input" checked={newData.isPriority}
+                 onChange={(e) => setNewData({ ...newData,isPriority: e.target.checked })} id="priorite" name="priorite" />
+                <label className="form-check-label" htmlFor="priorite">Prioritaire</label>
+              </div>
+              <textarea className="form-control mb-2" value={newData.description} onChange={handleChange} name="description"></textarea>
+              <input type="text" className="form-control mb-2" value={newData.lieu} onChange={handleChange} placeholder="Lieu" name="lieu" />
+            </div>
+          </div>
+        </div>
+      </form>
+    );
+}
+export default TodoForm;
