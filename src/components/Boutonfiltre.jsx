85be02fe51c00484c144df837cc6c491b965cdea
Romuald <nomenjanaharyandry.na@gmail.com>
2025-06-20 15:43:25 +0300

    Mise des boutons filtres en composant

---
+++ src/components/Boutonfiltre.jsx
@@ -0,0 +1,20 @@
+function Filtre({ onFiltre }) {
+
+  const boutonsFiltre = [
+    { fonction: 'All', label: 'Tous', nomBouton: 'Toutes'},
+    { fonction: 'completed', label: 'Complete', nomBouton: 'Complétées'},
+    { fonction: 'active', label: 'Active', nomBouton: 'Actives'},
+  ];
+
+  return(
+        <ul className="nav nav-tabs my-2 d-flex col-md-6 col-lg-4 col-xxl-3 justify-content-md-between">
+          { boutonsFiltre.map(({fonction, nomBouton}) => (
+	            <li key={fonction}>
+	            	<button className="btn-outline-success btn filtre" onClick={() => onFiltre(fonction)} >{nomBouton}</button>
+	            </li>
+	          ))
+    	  }
+        </ul>
+    );
+}
+export default Filtre;
